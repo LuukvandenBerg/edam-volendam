fe19fe9413736e60b4073d9363dec5178bd6bb53
LuukvandenBerg <luuk.berg1999@gmail.com>
2017-01-24 14:48:36 +0100

    mappen toegevoegd

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+//database login local
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'edam_volendam');
+define('DB_PASS', 'mysql');
+
+//database login online
+	
+?>
